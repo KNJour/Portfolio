69899e09acdc41c9574bd19b33755416d2484308
Keith Journell <keithj@Keiths-MBP-2.fios-router.home>
2022-02-10 17:53:03 -0500

    hopefully final changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <div className="container-fluid contentContainer relative">
             <div className="aboutRowBG"></div>
             <div className="row aboutRow mt-5 mb-5">
-                <div className="col-md-6 col-sm-12">
+                <div className="col-md-6 col-sm-12 aboutLeftCol">
                     <div className="row">
                         <div className="leftSideWrapper justify-content-between mb-3 keepAway">
                             <h2 className="myName ml-3">A little about me:</h2>
@@ -20,6 +20,7 @@ const About = () => {
                                     <div className="descriptor">Basically Steve Jobs</div>
                                 </div>
                             </div>
+                            <div className="buffer d-none d-lg-block"></div>
                             <div className="aboutMe mb-4 p-2">
                                 <p>After spending over 10 years in the service and insurance industries, I've finally transitioned to my life-long dream of <s>spending two hours looking for a typo</s> becoming a software developer. </p>
                                 <div className="buffer d-sm-none"></div>
@@ -32,10 +33,13 @@ const About = () => {
                             
                         </div>
                     </div>
-                    <div className="row aboutInfo d-flex align-items-center text-center justify-content-center mb-4 ml-1">
-                    <div className="buffer"></div>
-                        <div className="projectsWrapper p-4">
-                            <h4 className="smallDark">I am a full-stack software developer and a curious learner. I design modern websites, build fully functional software applications, and have experience making full CRUD applications powered by Python, JavaScript, React, and JAVA that manipulate and store data through MySQL and MongoDB.</h4>
+
+                    <div className="row aboutInfo d-flex text-center align-items-bottom justify-content-center mb-4 ml-1">
+                        <div className="col-12">
+                            <div className="buffer d-lg-none"></div>
+                            <div className=" p-4">
+                                <h4 className="smallDark">I am a full-stack software developer and a curious learner. I design modern websites, build fully functional software applications, and have experience making full CRUD applications powered by Python, JavaScript, React, and JAVA that manipulate and store data through MySQL and MongoDB.</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
